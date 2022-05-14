40f5407a9a1beba53bed57bbafb09e98e02299bf
iraceburu <79598754+iraceburu@users.noreply.github.com>
2022-05-14 19:05:58 +0200

    Add files via upload

---
+++ android.sh
@@ -0,0 +1,8 @@
+# Accedemos a la carpeta del proyecto
+cd webview-master
+# Vemos todas las tasks del graddle
+./gradlew tasks
+# Task de compilación y empaquetado
+./gradlew assembleDebug
+# Task de ejecución de tests
+./gradlew test
